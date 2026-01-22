2ff5054bfc2c89ac170ca56cf655fcd5e806c115
xiaoqi <82893987+2413181638@users.noreply.github.com>
2026-01-22 13:40:16 +0800

    Update changeallkey.sh

--- changeallkey.sh
+++ changeallkey.sh
@@ -14,8 +14,8 @@ export OLD_TOKEN='xqtx.okjnd.com'
 export NEW_TOKEN='xqtx.825332.xyz'
 
 # 同时修改 IP 协议
-OLD_URL='https://sytx.3651250.xyz'
-NEW_URL='https://sytx.354873.xyz'
+OLD_URL='https://xqtx.okjnd.com'
+NEW_URL='https://xqtx.825332.xyz'
 
 echo "🚀 开始执行强力替换..."
 
